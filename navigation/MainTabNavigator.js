4c1d04a98d717fc7cf0545be37be55ebe6b46aa7
Neil Vodoor <nvodoor@gmail.com>
2019-02-24 12:19:35 -0800

    Initialized Application

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import SearchScreen from '../screens/SearchScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const SearchStack = createStackNavigator({
+  Search: SearchScreen,
+});
+
+SearchStack.navigationOptions = {
+  tabBarLabel: 'Search',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'}
+    />
+  ),
+};
+
+const FavoritesStack = createStackNavigator({
+  Favorites: FavoritesScreen,
+});
+
+FavoritesStack.navigationOptions = {
+  tabBarLabel: 'Favorites',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  SearchStack,
+  FavoritesStack,
+});
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+
+export default class FavoritesScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Favorites',
+  };
+
+  render() {
+    <ScrollView>
+      
+    </ScrollView>
+  }
+}
